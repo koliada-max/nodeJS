4844ef0e2955c684b2e136ab5984ff20fd6a144f
Anastasiia Koliada <76648505+koliada-max@users.noreply.github.com>
2024-02-29 21:37:28 +0200

    lesson1 clean code

--- lesson1/util.js
+++ lesson1/util.js
@@ -1,10 +1,11 @@
 function generateHash(length) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
 
   for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      result += characters.charAt(randomIndex);
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
   }
 
   return result;
